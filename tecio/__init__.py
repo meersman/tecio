178d64360e4638aa2308b9e5408453f2f11ffc51
jmeersman <meersman.john@gmail.com>
2026-08-12 23:03:08 -0700

    bump to v0.2.4

--- tecio/__init__.py
+++ tecio/__init__.py
@@ -9,7 +9,7 @@ from importlib import metadata
 try:
     __version__ = metadata.version("tecio")
 except metadata.PackageNotFoundError:
-    __version__ = "0.2.3"
+    __version__ = "0.2.4"
 
 from . import cli, dat, libtecio, plt, szl
 from ._containers import VariableList, ZoneList
