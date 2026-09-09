d5a4f468df47bedae62edbe78a2d761ba75f3ea1
jmeersman <meersman.john@gmail.com>
2026-09-08 20:25:29 -0700

    fix version number

--- tecio/__init__.py
+++ tecio/__init__.py
@@ -9,7 +9,7 @@ from importlib import metadata
 try:
     __version__ = metadata.version("tecio")
 except metadata.PackageNotFoundError:
-    __version__ = "0.3.1"
+    __version__ = "0.3.2"
 
 from . import cli, libtecio
 from ._constants import (
